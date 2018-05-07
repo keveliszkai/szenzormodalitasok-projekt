4b75a578f55569badd3ccace9c67d75b0b625668
Keve <liszkaikeve@gmail.com>
2018-05-08 00:59:07 +0200

    [DIST] - 04

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://szenzormodalitasok.ddns.net:8080/',
+  apiUrl: 'http://szenzormodalitasok.ddns.net:8080',
   clientId: '1',
   clientSecret: 'client_secret'
 };
